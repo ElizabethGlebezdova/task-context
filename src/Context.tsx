6afd4228bccd8b2e15314946bcdee6da06733a06
ElizabethGlebezdova <elizabeth.glebezdova@yandex.ru>
2025-09-26 14:15:36 +0300

    Done

--- src/Context.tsx
+++ src/Context.tsx
@@ -1,10 +1,20 @@
-import { ReactElement } from 'react';
+import { ReactNode, createContext, useContext } from 'react';
 
 type Theme = 'light' | 'dark';
 
-export function ThemeProvider(props: {
-    theme: Theme;
-    children: ReactElement;
-}) {}
+const ThemeContext = createContext<Theme | undefined>(undefined);
 
-export function useTheme(): Theme {}
+export function ThemeProvider(props: { theme: Theme; children: ReactNode }) {
+    const { theme, children } = props;
+    return (
+        <ThemeContext.Provider value={theme}>{children}</ThemeContext.Provider>
+    );
+}
+
+export function useTheme(): Theme {
+    const theme = useContext(ThemeContext);
+    if (theme === undefined) {
+        throw new Error('useTheme must be used within a ThemeProvider');
+    }
+    return theme;
+}
